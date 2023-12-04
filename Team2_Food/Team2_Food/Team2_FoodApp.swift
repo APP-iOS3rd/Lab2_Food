af678e058bf0b0d8cb3953677d35264b5ab02aa7
NohJooYoung <nohjoo97@naver.com>
2023-12-04 15:24:28 +0900

    .DS_Store is banished

---
+++ Team2_Food/Team2_Food/Team2_FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team2_FoodApp.swift
+//  Team2_Food
+//
+//  Created by 노주영 on 2023/12/04.
+//
+
+import SwiftUI
+
+@main
+struct Team2_FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
