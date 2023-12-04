4fcefebac55864922fac421864ae2ee6cf701f27
hjsupernova <kem08120506@gmail.com>
2023-12-04 17:18:22 +0900

    Main View

--- Team2_Food/Team2_Food/RecipeType/RecipeTypeView.swift
+++ Team2_Food/Team2_Food/RecipeType/RecipeTypeView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct RecipeTypeView: View {
+    let recipeTypes: [String] = ["국", "반찬", "밥", "찌개", "후식"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    ForEach(recipeTypes, id: \.self) { type in
+                        Button {
+                            // API 호출
+                            // 예시 url http://openapi.foodsafetykorea.go.kr/api/acc5b06c942c4a94b3a1/COOKRCP01/json/1/15/RCP_PAT2=\(type)
+                        } label: {
+                            VStack {
+                                Image(type)
+                                    .resizable()
+                                    .scaledToFit()
+                                Text(type)
+                            }
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("레시피 종류")
+        }
     }
 }
 
