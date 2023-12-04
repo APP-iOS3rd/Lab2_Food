26475e0988e544a86fb5a8a0386cf85600c6ed71
NohJooYoung <nohjoo97@naver.com>
2023-12-04 17:30:43 +0900

    [Add] 레시피 목록 추가

--- Team2_Food/Team2_Food/RecipeInformation/RecipeInformationView.swift
+++ Team2_Food/Team2_Food/RecipeInformation/RecipeInformationView.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct RecipeInformationView: View {
+    
+    private let samples: [APIModel] = [
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00028_2.png", title: "새우두부계란찜"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00031_2.png", title: "방울토마토 소박이"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침"),
+        .init(image: "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00032_2.png", title: "오이무침")
+    
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button {
+                
+            } label: {
+                Text("<")
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+            }
+            .padding(.horizontal)
+
+            Spacer()
+            Text("Samples.Type")
+                .font(.largeTitle)
+                .bold()
+            
+            Spacer()
+            Spacer()
+        }
+        
+        ScrollView {
+            ForEach(samples, id: \.self) { sample in
+                HStack {
+                    AsyncImage(url: URL(string: sample.image)!) {
+                        image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(12.0)
+                            .frame(width: 80, height: 80)
+                    } placeholder: {
+                        ProgressView()
+                            .frame(width: 80, height: 80)
+                    }
+                    
+                    Text(sample.title)
+                        .font(.system(size: 25))
+                    
+                    Spacer()
+                }
+                .padding(.leading, 20)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
     RecipeInformationView()
 }
+
